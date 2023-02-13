325e9b1e18766167528d1b28c2069841b886b033
MetallAnother <mstytely4@gmail.com>
2023-02-13 02:36:54 +0200

    lol

--- skila2/corona.js
+++ skila2/corona.js
@@ -1,20 +1,69 @@
-let a = 0 ;
 
+let a = -1200 ;
 
+let c = 1200;
 
  document.querySelector('.but2-right').addEventListener('click', () => {
     a = a + 400 ;
-    if (a > 1200) {
-        a = 0
+    if (a > 0 ) {
+        a = -800
     }
-    document.querySelector('.slide-line').style.left = -a + 'px' ;
+    console.log(a)
+    document.querySelector('.slide-line').style.left = a + 'px' ;
 })
  document.querySelector('.but2-left').addEventListener('click', () => {
     a = a - 400 ;
-    if (a < 0) {
-        a = 1200
+    if (a < -2400) {
+        a = -400
+    
     }
-    document.querySelector('.slide-line').style.left = -a + 'px' ;
+    console.log(a)
+    document.querySelector('.slide-line').style.left = a + 'px' ;
 })
 
+
+
+let sln1 = document.querySelector('.slide-line1');
+
+
+
+
+let b = 0 ;
+
+document.querySelector('.but1-right').disabled = true;
+document.querySelector('.but1-left').disabled = true;
+
+
+document.querySelector('.but1-right').addEventListener('click', () => {
+    b = b + 1707 ;
+    if (b > 3414) {
+        b = 0
+    }
+    sln1.style.left = -b + 'px' ;
+    console.log(b)
+})
+document.querySelector('.but1-left').addEventListener('click', () => {
+    b = b - 1707 ;
+    if (b < 0) {
+        b = 3414
+    }
+    document.querySelector('.slide-line1').style.left = -b + 'px' ;
+})
+
+
+let d = 0;
+
+document.querySelector('.bur-but').addEventListener('click', () =>{
+
+    if(d == 0){
+        document.querySelector('.bur-men').style.display = 'flex';
+        d=1
+    }
+    else{
+        document.querySelector('.bur-men').style.display = 'none';
+        d=0
+    }
+
+
+})
 
